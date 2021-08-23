553fb4ab6132369522dd42c046e9a368bf6164c2
Steven Quintana <steve.e.quint@gmail.com>
2021-08-23 16:52:25 -0700

    Load no data message if there is none for that specific array depending on toggle button change

--- assets/js/sessions-script.js
+++ assets/js/sessions-script.js
@@ -1,4 +1,9 @@
 
+// Create array for favorites and history from local storage.
+// If no data yet exists, create and empty array.
+let sessionsFavorites = JSON.parse(localStorage.getItem('sessionsFavorites')) || []
+let sessionsHistory = JSON.parse(localStorage.getItem('sessionsHistory')) || []
+
 // Variable to hold value of whether or not the favorite button has been clicked.
 let toggle = false
 
@@ -16,6 +21,15 @@ document.getElementById('fav-hist-toggle').addEventListener('click', event => {
     <i class="heart outline icon"></i>
     Favorite Sessions
     `
+    
+    console.log(sessionsFavorites)
+    // If there is no favorites data, load no favorites message.
+    if (sessionsFavorites.length === 0) {
+      document.getElementById('session-message').innerHTML = `
+      <p class="no-data-message">You don't have any favorite sessions saved yet.</p>
+      <p class="no-data-message message-2">Click on the like button after a session to add it to favorites.</p>
+      `
+    }
 
     // Set toggle to true.
     toggle = true
@@ -31,6 +45,16 @@ document.getElementById('fav-hist-toggle').addEventListener('click', event => {
     <i class="history icon"></i>
     History
     `
+
+    console.log(sessionsHistory)
+
+    // If there is no history data, load no history message.
+    if (sessionsHistory.length === 0) {
+      document.getElementById('session-message').innerHTML = `
+      <p class="no-data-message">You don't have any sessions recorded yet.</p>
+      <p class="no-data-message message-2">Use our sessions finder above then get out there and surf!</p>
+      `
+    }
 
     // Set toggle to false.
     toggle = false
