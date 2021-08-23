7cf582d37822dc60582f7f9f2873f5bad35b9f48
Steven Quintana <steve.e.quint@gmail.com>
2021-08-23 16:30:40 -0700

    Toggle divider line content in toggle click function

--- assets/js/sessions-script.js
+++ assets/js/sessions-script.js
@@ -5,16 +5,32 @@ let toggle = false
 // Toggle back and forth between history information and favorite info on click.
 document.getElementById('fav-hist-toggle').addEventListener('click', event => {
 
-  // If toggle is false, change to history information.
+  // If toggle is false, change to favorite information.
   if (!toggle) {
+    
+    // Change button text.
     document.getElementById('fav-hist-toggle').textContent = 'History'
+
+    // Change divider icon and title.
+    document.getElementById('sessions-divider').innerHTML = `
+    <i class="heart outline icon"></i>
+    Favorite Sessions
+    `
 
     // Set toggle to true.
     toggle = true
   }
-  // Otherwise, if toggle is true change inforation to favorite.
+  // Otherwise, if toggle is true change inforation to history.
   else {
+
+    // Change button text.
     document.getElementById('fav-hist-toggle').textContent = 'Favorite Sessions'
+
+    // Change divider icon and title.
+    document.getElementById('sessions-divider').innerHTML = `
+    <i class="history icon"></i>
+    History
+    `
 
     // Set toggle to false.
     toggle = false
