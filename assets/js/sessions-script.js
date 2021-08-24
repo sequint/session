2a06caff11251e6ae05772cdb5e22a27bb0acc9c
Steven Quintana <steve.e.quint@gmail.com>
2021-08-24 09:20:20 -0700

    Put foreach iterator to loop through history inside conditional

--- assets/js/sessions-script.js
+++ assets/js/sessions-script.js
@@ -90,7 +90,6 @@ document.getElementById('fav-hist-toggle').addEventListener('click', event => {
     Favorite Sessions
     `
     
-    console.log(sessionsFavorites)
     // If there is no favorites data, load no favorites message.
     if (sessionsFavorites.length === 0) {
       document.getElementById('session-message').innerHTML = `
@@ -142,13 +141,48 @@ document.getElementById('fav-hist-toggle').addEventListener('click', event => {
       `
     }
     else {
-      document.getElementById('session-message').innerHTML = `
-      <div class="ui very padded segment">
-        <p></p>
-      </div>
-      `
+      sessionsHistory.forEach(session => {
+        document.getElementById('session-message').innerHTML = `
+        <div class="ui very padded segment">
+          <div class="ui card">
+            <div class="content">
+              <div class="header">${session.date}</div>
+            </div>
+            <div class="content">
+              <h4 class="ui sub header">${session.location}</h4>
+              <div class="ui small feed">
+                <div class="event">
+                  <div class="content">
+                    <div class="summary">
+                      <p>Wave Height: ${session.waveHeight} ft.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="event">
+                  <div class="content">
+                    <div class="summary">
+                      <p>Water Temp: ${session.waterTemp} degrees</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="event">
+                  <div class="content">
+                    <div class="summary">
+                      <p>Restaurant: ${session.restaurant}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="extra content like-dislike-area">
+              <i class="big heart icon like"></i>
+              <i class="big close icon dislike"></i>
+            </div>
+          </div>
+        </div>
+        `
+      })
     }
-
     // Set toggle to false.
     toggle = false
   }
