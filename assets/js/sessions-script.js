222a0a51fe68611fd56be618580d1639cf4770d8
jypkr <wodusqkr1210@gmail.com>
2021-08-26 11:40:36 -0700

    0826-1140

--- assets/js/sessions-script.js
+++ assets/js/sessions-script.js
@@ -524,84 +524,70 @@ document.addEventListener('click', event => {
 document.addEventListener('click', event => {
   if (event.target.classList.contains('go-btn')) {
 
-    const latitude = sessionStorage.getItem('latitude')
-    const longitude = sessionStorage.getItem('longitude')
-    //const cuisine = sessionsPreferences.getItem('food_type')
-
-    console.log(event.target.parentNode.parentNode)
-
-    axios.get(`https://api.documenu.com/v2/restaurants/search/geo?lat=${latitude}&lon=${longitude}&distance=1`, {
-      headers: {
-        'X-API-KEY': '65e9991ec80a9970fe3112ddc2617c8b'
-      }
-    })
-      .then(res => {
-        const restaurant_info = res.data.data
-        console.log(restaurant_info)
-        localStorage.setItem('restaurant_info', JSON.stringify(restaurant_info))
-      })
-      .catch(error => console.log(error))
+    const latitude = event.target.parentNode.parentNode.children[0].dataset.lat
+    const longitude = event.target.parentNode.parentNode.children[0].dataset.long
+    const cuisine = sessionsPreferences.getItem("food_type")
 
     // // Default preferences for food type
-    // if (cuisine === "default") {
-    //   axios.get(`https://api.documenu.com/v2/restaurants/search/geo?lat=${latitude}&lon=${longitude}&distance=5`, {
-    //     headers: {
-    //       'X-API-KEY': '65e9991ec80a9970fe3112ddc2617c8b'
-    //     }
-    //   })
-    //     .then(res => {
-    //       const restaurant_info = res.data.data
-    //       localStorage.setItem('restaurant_info', JSON.stringify(restaurant_info))
-    //     })
-    //     .catch(error => console.log(error))
-    // }
-    // // specific cuisine preferences
-    // else {
-    //   axios.get(`https://api.documenu.com/v2/restaurants/search/geo?lat=${latitude}&lon=${longitude}&distance=5&cuisine=${cuisine}`, {
-    //     headers: {
-    //       'X-API-KEY': '65e9991ec80a9970fe3112ddc2617c8b'
-    //     }
-    //   })
-    //     .then(res => {
-    //       const restaurant_info = res.data.data
-    //       localStorage.setItem('restaurant_info', JSON.stringify(restaurant_info))
-    //     })
-    //     .catch(error => console.log(error))
-    // }
+    if (cuisine === "default") {
+      axios.get(`https://api.documenu.com/v2/restaurants/search/geo?lat=${latitude}&lon=${longitude}&distance=5`, {
+        headers: {
+          'X-API-KEY': '65e9991ec80a9970fe3112ddc2617c8b'
+        }
+      })
+        .then(res => {
+          const restaurant_info = res.data.data
+          localStorage.setItem('restaurant_info', JSON.stringify(restaurant_info))
+        })
+        .catch(error => console.log(error))
+    }
+    // specific cuisine preferences
+    else {
+      axios.get(`https://api.documenu.com/v2/restaurants/search/geo?lat=${latitude}&lon=${longitude}&distance=5&cuisine=${cuisine}`, {
+        headers: {
+          'X-API-KEY': '65e9991ec80a9970fe3112ddc2617c8b'
+        }
+      })
+        .then(res => {
+          const restaurant_info = res.data.data
+          localStorage.setItem('restaurant_info', JSON.stringify(restaurant_info))
+        })
+        .catch(error => console.log(error))
+    }
 
     // Display list of restaurants and let user choose one
-    //displayRestaurants()
+    displayRestaurants()
   }
 })
 
 const displayRestaurants = () => {
 
   console.log(restaurant_info)
-  // // If there is no favorites data, load no favorites message.
-  // if (restaurant_info.length === 0) {
-  //   document.getElementById('restaurants-main-display').innerHTML = `
-  //     <div id="popup1" class="overlay">
-  //       <div class="popup">
-  //         <h2>Here i am</h2>
-  //         <a class="close" href="#">&times;</a>
-  //         <div class="content">
-  //           Thank to pop me out of that button, but now i'm done so you can close this window.
-  //         </div>
-  //       </div>
-  //     </div>
-  //     `
-  // }
-  // else {
-  //   document.getElementById('restaurants-main-display').innerHTML = `
-  //     <div id="popup1" class="overlay">
-  //       <div class="popup">
-  //         <h2>Here i am</h2>
-  //         <a class="close" href="#">&times;</a>
-  //         <div class="content">
-  //           Thank to pop me out of that button, but now i'm done so you can close this window.
-  //         </div>
-  //       </div>
-  //     </div>
-  //   `
-  // }
+  // If there is no favorites data, load no favorites message.
+  if (restaurant_info.length === 0) {
+    document.getElementById('restaurants-main-display').innerHTML = `
+      <div id="popup1" class="overlay">
+        <div class="popup">
+          <h2>Here i am</h2>
+          <a class="close" href="#">&times;</a>
+          <div class="content">
+            Thank to pop me out of that button, but now i'm done so you can close this window.
+          </div>
+        </div>
+      </div>
+      `
+  }
+  else {
+    document.getElementById('restaurants-main-display').innerHTML = `
+      <div id="popup1" class="overlay">
+        <div class="popup">
+          <h2>Here i am</h2>
+          <a class="close" href="#">&times;</a>
+          <div class="content">
+            Thank to pop me out of that button, but now i'm done so you can close this window.
+          </div>
+        </div>
+      </div>
+    `
+  }
 }
